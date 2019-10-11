e0443a7fa5635550d09a5a5a6b7a9b27b01e5ffa
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2019-10-11 10:26:32 -0700

    Initial commit

---
+++ Assets/DemonstrateConvexHull.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent (typeof(WorldDrawer))]
+public class DemonstrateConvexHull : MonoBehaviour
+{
+    public int cloudSize;
+    public int cloudScale;
+    WorldDrawer drawer;
+    // lets make the bounds of the cloud -1M to +1M in x and y
+    // Start is called before the first frame update
+    void Start()
+    {
+        drawer = GetComponent<WorldDrawer>();
+        Vector2Int[] cloud = new Vector2Int[cloudSize];
+        for(int i = 0 ; i < cloudSize; i++)
+        {
+            cloud[i] = new Vector2Int(UnityEngine.Random.Range(-cloudScale,cloudScale) , UnityEngine.Random.Range(-cloudScale,cloudScale));
+        }
+
+        Array.Sort(cloud, Vector2IntComparer); //ascending or descending?
+        List<Vector2Int> remainder = new List<Vector2Int>();
+        List<Vector2Int> hull = ComputeHull(cloud, remainder);
+        foreach(Vector2Int v in remainder)
+            drawer.DrawPoint((float)(v.x)/cloudScale , (float)(v.y)/cloudScale , Color.blue);
+        foreach(Vector2Int v in hull)
+            drawer.DrawPoint((float)(v.x)/cloudScale , (float)(v.y)/cloudScale , Color.red);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<Vector2Int> ComputeHull(Vector2Int[] cloud, List<Vector2Int> remainder)
+    {
+        List<Vector2Int> hull = new List<Vector2Int>();
+        for( int i = 0 ; i < cloudSize ; i++)
+        {
+            hull.Add(cloud[i]);
+            if(hull.Count<3)
+                continue;
+            while(CalcArea(hull[hull.Count-3] , hull[hull.Count-2] , hull[hull.Count-1]) <= 0)
+            {
+                hull.RemoveAt(hull.Count-2);
+                if(hull.Count<3)
+                    break;
+            }            
+        }
+        //upper hull done
+        int upperSize = hull.Count;
+        for(int i = cloudSize-2; i>=0 ; i--)
+        {
+            hull.Add(cloud[i]);
+            if(hull.Count<2 + upperSize)
+                continue;
+            while(CalcArea(hull[hull.Count-3] , hull[hull.Count-2] , hull[hull.Count-1]) <= 0)
+            {
+                if(findIndexOf(hull[hull.Count-2], hull, 0 , upperSize) ==-1) //ruins n log n running time
+                    remainder.Add(hull[hull.Count-2]);
+                hull.RemoveAt(hull.Count-2);
+                if(hull.Count<3 + upperSize)
+                    break;
+            }     
+        }
+        if(hull[0] == hull[hull.Count-1])
+            hull.RemoveAt(hull.Count-1);
+        return hull;
+    }
+
+    int findIndexOf( Vector2Int v,List<Vector2Int> l, int start, int finish)
+    {
+        for(int i = start ; i < finish ; i++)
+        {
+            if(l[i] == v)
+                return i;
+        }
+        return -1;
+    }
+
+    public class V2iComparerHelper : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            Vector2Int a = (Vector2Int)x;
+            Vector2Int b = (Vector2Int)y;
+            if(a.x == b.x)
+                return a.y-b.y;
+            return a.x-b.x;
+        }
+        
+    }
+
+    public int Vector2IntComparer(Vector2Int a, Vector2Int b)
+    {
+        if(a.x == b.x)
+            return a.y-b.y;
+        return a.x-b.x;
+    }
+
+
+    double CalcArea(Vector2Int a, Vector2Int b, Vector2Int c) //clockwise right turn should return positive x.
+    {
+        return (b.x-a.x) * (double)(b.y+a.y) + (c.x-b.x) * (double)(c.y+b.y) + (a.x-c.x)*(double)(a.y+c.y);
+    }
+
+}
---
+++ Assets/WorldDrawer.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class WorldDrawer : MonoBehaviour
+{
+    
+    /*so i want the algorithms to be able to take a suite of actions, using a master class to do things to the world.
+    everything should be recorded in a c# list, and the user should be able to step by step advance the algorithm and rewind it.
+    
+    each method must be atomic, and invertible. 
+    */
+
+    List<System.Action<object[]>> steps;
+    List<System.Action<object[]>> inverses;
+    List<object[]> arguments;
+    List<List<object>> data;
+    int Index;
+    void Awake()
+    {
+        Index = 0;
+        steps = new List<Action<object[]>>();
+        inverses = new List<Action<object[]>>();
+        arguments = new List<object[]>();
+        data = new List<List<object>>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+            Advance();
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+            Reverse();
+    }
+
+    void Advance()
+    {
+        steps[Index](arguments[Index]);
+        Index ++;
+    }
+
+    void Reverse()
+    {
+        Index--;
+        inverses[Index](arguments[Index]);
+    }
+
+    public void DrawPoint(float x, float y, Color c)
+    {
+        steps.Add(_DrawPoint);
+        inverses.Add(_DrawPointInverse);
+        arguments.Add( new object[]{x,y,c});
+        data.Add(new List<object>());
+    }
+
+    private void _DrawPoint(params object[] args)
+    {   
+        float x = (float)args[0];
+        float y = (float)args[1];
+        Color c = (Color)args[2];
+         //Create a new cube primitive to set the color on
+        GameObject s = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        s.transform.position = new Vector3(x,y);
+        s.transform.localScale = new Vector3(.1f,.1f,.1f);
+        data[Index].Add(s);
+        //Get the Renderer component from the new cube
+        /*var cubeRenderer =*/s.GetComponent<Renderer>().material.SetColor("_Color", c);
+        //cube.GetComponent<Material>().EnableKeyword("_Color");//might need to do this or something
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        //cubeRenderer.material.SetColor("_Color", c);
+
+
+    }
+
+    private void _DrawPointInverse(params object[] args)
+    {
+        float x = (float)args[0];
+        float y = (float)args[1];
+        Color c = (Color)args[2];
+        GameObject g = (GameObject)data[Index][0];
+        Destroy(g);
+        data[Index].Clear();
+    }
+}
