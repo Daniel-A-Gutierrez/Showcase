39412ca48d3a8198ebdea1b5854e454b839513cc
Daniel-A-Gutierrez <icanmakespaghetti@gmail.com>
2019-12-14 14:04:58 -0800

    tlmh kinda

---
+++ Assets/TLMH mech redo/Hook.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    public float speed;
+    public float maxDistance;
+    public float minDistance;
+    public bool affixed = false;
+    public bool canAffix = true;
+    public bool outgoing = true;
+    public Spodermun Controller;
+    Vector3 affixedOffset;
+    Transform affixedTo;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right *speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(affixed)
+                transform.position = affixedTo.position - affixedOffset;
+        if(outgoing)
+        {
+            if(  (transform.position - Controller.transform.position).magnitude > maxDistance )
+                outgoing = false;    
+        }
+        else if (!affixed)
+        {
+            rb.velocity =  ( Controller.transform.position - transform.position ).normalized*speed;
+            transform.right = -rb.velocity.normalized;
+            if((transform.position - Controller.transform.position).magnitude < minDistance)
+            {
+                Controller.DestroyHook();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void Release()
+    {   
+        if(affixed)
+            canAffix = false;
+        affixed = false;
+        affixedTo = null;
+        rb.constraints = RigidbodyConstraints2D.None;
+        outgoing  = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(canAffix)
+        {
+            affixed = true;
+            affixedTo =col.transform;
+            affixedOffset = affixedTo.position - transform.position;
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+}
---
+++ Assets/TLMH mech redo/TemplatePlayerController.cs
@@ -0,0 +1,293 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TemplatePlayerController : MonoBehaviour
+{
+
+    Dictionary<string,Action> States;
+    [SerializeField]
+    string state = "DefaultState";
+    Rigidbody2D rb;
+
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+    public KeyCode jump = KeyCode.Space;
+
+    public float walkSpeed = 4;
+    public float airControl = .1f;
+    public float jumpMaxHeight = 250;//idk what the fuck units these are but it works
+    public float jumpMinHeight = 150;
+    public float jumpCooldown = .1f;
+    public float gravityScale = .5f;
+    public float maxAirSpeed = 16;
+    public float fallSpeedCap = 32;
+    public float RoofCheckOffset = .32f;
+    public float GroundCheckOffset =.38f;
+    
+    public Vector2 RoofCheckBounds;
+    public Vector2 GroundCheckBounds;
+    public string groundLayer;
+    public string roofLayer;
+
+    float currentGravity;
+    bool gravityOn = true;
+    bool grounded;
+    bool roofed;
+    float lastJumpTime;
+    Vector2 moveVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        States = new Dictionary<string, Action>();
+        rb = GetComponent<Rigidbody2D>();
+        States["DefaultState"] = DefaultState;
+        States["JumpingState"] = JumpingState;
+        States["FallingState"] = FallingState;
+        ResetGravity();
+    }
+
+    void FixedUpdate()
+    {
+        if(moveVec!=Vector2.zero)
+            rb.MovePosition(rb.position+moveVec);
+    }
+
+    void Update()
+    {
+        States[state]();
+    }
+
+
+
+
+
+
+//states
+
+    void EnterDefaultState()
+    {
+        state = "DefaultState";
+        DefaultState();
+    }
+    void DefaultState()
+    {
+        SetMoveDir();
+        CheckGrounded();
+        CheckRoofed();
+        ApplyGravity();
+        if(!roofed && Input.GetKeyDown(jump)&&Time.time-lastJumpTime > jumpCooldown)
+        {
+            ExitDefaultState();
+            EnterJumpingState();
+            return;
+        }
+        if(!grounded)
+        {
+            ExitDefaultState();
+            EnterFallingState();
+            return;
+        }
+    }
+    void ExitDefaultState()
+    {
+
+    }
+
+    void EnterJumpingState()
+    {
+        lastJumpTime = Time.time;
+        state = "JumpingState";
+        //max height is going to be under lowered gravity, min under normal gravity.
+        /*so v = root( 2ax ) */
+        moveVec.y = Mathf.Sqrt(2 * gravityScale * jumpMinHeight)*Time.fixedDeltaTime;
+        //but now i want it to only cancel once its reached max height
+        currentGravity = moveVec.y *moveVec.y /  (2*jumpMaxHeight*Time.fixedDeltaTime*Time.fixedDeltaTime);
+        JumpingState();
+    }
+    void JumpingState()
+    {
+        if(!Input.GetKey(jump))
+        {
+            ResetGravity();
+        }
+        CheckRoofed();
+        CheckGrounded();
+        
+        if(grounded && Time.time - lastJumpTime > jumpCooldown)
+        {
+            ExitJumpingState();
+            EnterDefaultState();
+            return;
+        }
+
+        if(roofed || moveVec.y < 0)
+        {
+            ExitJumpingState();
+            EnterFallingState();
+            return;
+        }
+
+        ApplyGravity();
+        SetMoveDirAir();
+    }
+    void ExitJumpingState()
+    {
+        ResetGravity();
+        moveVec.y = 0;
+    }
+
+    void EnterFallingState()
+    {
+        state = "FallingState";
+        FallingState();
+    }
+    void FallingState()
+    {
+        CheckGrounded();
+        CheckRoofed();
+        ApplyGravity();
+        if(roofed)
+        {
+            ApplyGravity();
+        }
+        SetMoveDirAir();
+        if(grounded)
+        {
+            ExitFallingState();
+            EnterDefaultState();
+            return;
+        }
+    }
+    void ExitFallingState()
+    {
+        moveVec.y = 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//useful functions
+    float FloatComp(float a, float b, float tolerance = .000001f)
+    {
+        if(float.IsNaN(a) || float.IsNaN(b))
+            return float.NaN;
+        else if(float.IsInfinity(a) && float.IsInfinity(b))
+            return float.PositiveInfinity;
+        else if( Mathf.Abs(Mathf.Abs(a) - Mathf.Abs(b)) < tolerance)
+            return 0f;
+        else if( a - b > tolerance)
+            return 1f;
+        else if (b-a > tolerance)
+            return -1f;
+        else
+            return float.NaN;
+
+    }
+
+    Collider2D[] colliders;
+    bool CheckBoxOverlap( Vector2 center, Vector2 size, float rotation, LayerMask checkLayers)
+    {
+        if(colliders == null)
+            colliders = new Collider2D[24];
+        ContactFilter2D c = new ContactFilter2D();
+        c.layerMask = checkLayers;
+        c.useLayerMask = true;
+        return Physics2D.OverlapBox(center,size, rotation,c,colliders) > 0;
+    }
+
+
+    void ZeroMoveDir()
+    {
+        moveVec= Vector2.zero;
+    }
+
+    //moveVec
+    void SetMoveDir()
+    {
+        moveVec = ((Input.GetKey(left) ? Vector2.left : Vector2.zero) +
+         (Input.GetKey(right) ? Vector2.right : Vector2.zero)).normalized *walkSpeed*Time.fixedDeltaTime;
+    }
+
+    //applies input to moveVec without resetting Y. takes air control into account. 
+    void SetMoveDirAir()
+    {
+       
+        float apply = ((Input.GetKey(left) ? -1 : 0) +
+            (Input.GetKey(right) ? 1 : 0))*walkSpeed*Time.fixedDeltaTime*airControl;
+        if(  FloatComp( Mathf.Abs(moveVec.x) , Mathf.Abs(maxAirSpeed * Time.fixedDeltaTime)  ) < 0)
+            {moveVec.x += apply;}
+        else if( FloatComp(  moveVec.x , maxAirSpeed*Time.fixedDeltaTime) >= 0  && apply < 0) //allow decelleration but not accelleration above max. 
+            {moveVec.x += apply; }
+        else if( FloatComp ( moveVec.x , -maxAirSpeed*Time.fixedDeltaTime) <= 0 && apply > 0)
+            {moveVec.x += apply;}
+    }
+
+    //moveVec
+    void ApplyGravity() //rn gravity is linear
+    {
+        if(gravityOn && moveVec.y >= -fallSpeedCap*Time.fixedDeltaTime && !grounded)
+            moveVec += Vector2.down * currentGravity * Time.fixedDeltaTime;
+    }
+
+    //grounded
+    void CheckGrounded()
+    {
+        grounded = CheckBoxOverlap(transform.position + Vector3.up*GroundCheckOffset, GroundCheckBounds, 0f, 1 <<LayerMask.NameToLayer( groundLayer ));
+    }
+
+    //roofed
+    void CheckRoofed()
+    {
+        roofed = CheckBoxOverlap( transform.position + Vector3.up*RoofCheckOffset, RoofCheckBounds, 0f, 1 << LayerMask.NameToLayer( roofLayer ));
+    }
+
+    //gravityScale , lastGravity
+    void GravityOn()
+    {
+        currentGravity = gravityScale;
+    }
+
+    //gravityScale, lastGravity
+    void GravityOff()
+    {
+        currentGravity = 0;
+    }
+
+    //gravityScale, lastGravity
+    void SetGravity(float g)
+    {
+        if(g!=currentGravity)
+        {
+            currentGravity = g;
+        }
+    }
+
+    void ResetGravity()
+    {
+        currentGravity = gravityScale;
+    }
+
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position + Vector3.up*GroundCheckOffset , GroundCheckBounds);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position + Vector3.up*RoofCheckOffset , RoofCheckBounds);
+    }
+
+}
